1eed8336fa10c410f7b92c57570c500ee317cfa9
dog <dog@dog.com>
2024-08-18 16:11:23 +0500

    Refactor.\nAdd functionality to add some sites to be remembered so its cookies won't be deleted after  relaunching firefox

--- roles/firefox/files/firefox_bookmark.py
+++ roles/firefox/files/firefox_bookmark.py
@@ -1,39 +1,48 @@
 #!/usr/bin/env python
 
-
 import os
 import sqlite3
-from pathlib import Path
 import shutil
+import time
 
-username = os.getlogin()
 
-places_path = "places.sqlite"
-db = sqlite3.connect(places_path)
+def create_bookmarks():
+    username = os.getlogin()
+    places_path = "places.sqlite"
+    db = sqlite3.connect(places_path)
+    categories = [
+        "Social", "Tech", "Work", "Research", "Health", "Education",
+        "Tools", "Blogs", "Music", "Books", "Reference", "Learning",
+        "Hobbies", "To Read", "Misc."
+    ]
+    for i, category in enumerate(categories):
+        db.execute(f"INSERT INTO moz_bookmarks (type, title, position, parent)\
+                     VALUES ('2', '{category}', '{i}', '3');")
+    db.commit()
+    db.close()
+    shutil.copyfile("./places.sqlite", f"/home/{username}/.mozilla/firefox/{username}.default/places.sqlite")
+    shutil.copyfile("./places.sqlite.original", "./places.sqlite")
 
-categories = [
-    "Social",
-    "Tech",
-    "Work",
-    "Research",
-    "Health",
-    "Education",
-    "Tools",
-    "Blogs",
-    "Music",
-    "Books",
-    "Reference",
-    "Learning",
-    "Hobbies",
-    "To Read",
-    "Misc."
-]
-for i, category in enumerate(categories):
-    db.execute(f"INSERT INTO moz_bookmarks (type, title, position, parent)\
-                 VALUES ('2', '{category}', '{i}', '3');")
+def delete_previous_remembered_websites():
+    username = os.getlogin()
+    permission_path = f"/home/{username}/.mozilla/firefox/{username}.default/permissions.sqlite"
+    db = sqlite3.connect(permission_path)
+    db.execute("DELETE FROM moz_perms WHERE origin LIKE 'https://%' OR  origin LIKE 'http://%'")
+    db.commit()
+    db.close()
+def add_remembered_websites():
+    username = os.getlogin()
+    websites = ["open.spotify.com", "twitter.com", "github.com", "gitlab.com", "web.whatsapp.com"]
+    permission_path = f"/home/{username}/.mozilla/firefox/{username}.default/permissions.sqlite"
+    db = sqlite3.connect(permission_path)
+    for website in websites:
+        db.execute(f"INSERT INTO moz_perms (origin, type,permission,expireType)\
+                VALUES ('https://www.{website}', 'cookie', '1', '0');")
+    db.commit()
+    db.close()
 
-print(*db.execute("SELECT title FROM moz_bookmarks ;").fetchall())
-db.commit()
-db.close()
-shutil.copyfile("./places.sqlite", f"/home/{username}/.mozilla/firefox/{username}.default/places.sqlite")
-shutil.copyfile("./places.sqlite.original", "./places.sqlite")
+
+if __name__ == "__main__":
+    delete_previous_remembered_websites()
+    add_remembered_websites()
+    create_bookmarks()
